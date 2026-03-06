81754cfb34cc69fe25131f8793e3bc46a5e62e96
LogiCule <rahuldubey7398@gmail.com>
2026-03-06 23:30:20 +0530

    factorial added

--- src/test/java/com/calculator/Calculator.java
+++ src/test/java/com/calculator/Calculator.java
@@ -11,8 +11,20 @@ public class Calculator {
         return Math.sqrt(x);
     }
 
-    public static double factorial(int x) {
-        return 0;
+    public static long factorial(long n) {
+
+        if (n < 0) {
+            throw new ArithmeticException("Factorial of negative number not allowed");
+        }
+
+        long MOD = 1000000007L;
+        long result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result = (result * i) % MOD;
+        }
+
+        return result;
     }
 
     public static double naturalLog(double x) {
